2a5f8976777cf1fa5665c4c3926a28861928fb35
DalThePal <dallen.hoyal@gmail.com>
2018-07-25 12:04:45 -0600

    made it mobile responsive

--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,24 @@ export default class About extends Component {
             <div className='About'>
                 <div className='background-img'></div>
                 <div className='profile-pic'></div>
-                <div className='first-name'>DALLEN</div>
-                <div className='last-name'>HOYAL</div>
                 <div className='profile'>
+                    <div className='full-name'>
+                        <div className='first-name'>DALLEN</div>
+                        <div className='last-name'>HOYAL</div>
+                    </div>
                     <div className='profile-content' ref='about'>
                         <h1 className='about-header'>ABOUT ME</h1>
                         <p className='about-paragraph'>
-                            I am a Junior Web Developer that strives for excellence in everything I do.   My 
-                            pursuit of Web Development stems from my passion of creating. I love to create, 
-                            tear apart, and fix things.  But what I enjoy more than creating is discovering 
+                            I am a Junior Web Developer that strives for excellence in everything I do.   My
+                            pursuit of Web Development stems from my passion of creating. I love to create,
+                            tear apart, and fix things.  But what I enjoy more than creating is discovering
                             new ways to create and exploring the world I live in.
                         </p>
                         <p className='about-paragraph'>
-                            Whenever I am asked what my hobbies are, I often find myself explaining that my 
-                            hobby is finding new hobbies. I've explored knife making, restoring old motorcycles, 
-                            painting miniatures, building my own gameboy, mushroom hunting, and so many more.  I 
-                            just love learning new things and in Web Development, there is always something new to 
+                            Whenever I am asked what my hobbies are, I often find myself explaining that my
+                            hobby is finding new hobbies. I've explored knife making, restoring old motorcycles,
+                            painting miniatures, building my own gameboy, mushroom hunting, and so many more.  I
+                            just love learning new things and in Web Development, there is always something new to
                             learn.
                         </p>
                     </div>
