c2df0cb72c78cae9389c8df7c239204027c5b7a7
DalThePal <dallen.hoyal@gmail.com>
2018-10-19 08:11:44 -0600

    centering about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,8 @@ export default class About extends Component {
     render() {
         return (
             <div className='About'>
-                
                 <div className='background-img'></div>
-                
-                <div className='profile'>
+                <div className='profile-gap'>
                     <div className='f_name-wrapper'>
                         <div className='first-name'>DALLEN</div>
                     </div>
@@ -18,23 +16,24 @@ export default class About extends Component {
                     <div className='pic-wrapper'>
                         <div className='profile-pic'></div>
                     </div>
-                    <div className='profile-content' ref='about'>
-                        <h1 className='about-header'>ABOUT ME</h1>
-                        <p className='about-paragraph'>
-                            I am a Junior Web Developer that strives for excellence in everything I do.   My
-                            pursuit of Web Development stems from my passion of creating. I love to create,
-                            tear apart, and fix things.  But what I enjoy more than creating is discovering
-                            new ways to create and exploring the world I live in.
-                        </p>
-                        <p className='about-paragraph'>
-                            Whenever I am asked what my hobbies are, I often find myself explaining that my
-                            hobby is finding new hobbies. I've explored knife making, restoring old motorcycles,
-                            painting miniatures, building my own gameboy, mushroom hunting, and so many more.  I
-                            just love learning new things and in Web Development, there is always something new to
-                            learn.
-                        </p>
-                    </div>
                 </div>
+                <div className='profile-content' ref='about'>
+                    <h1 className='about-header'>ABOUT ME</h1>
+                    <p className='about-paragraph'>
+                        I am a Junior Web Developer that strives for excellence in everything I do.   My
+                        pursuit of Web Development stems from my passion of creating. I love to create,
+                        tear apart, and fix things.  But what I enjoy more than creating is discovering
+                        new ways to create and exploring the world I live in.
+                    </p>
+                    <p className='about-paragraph'>
+                        Whenever I am asked what my hobbies are, I often find myself explaining that my
+                        hobby is finding new hobbies. I've explored knife making, restoring old motorcycles,
+                        painting miniatures, building my own gameboy, mushroom hunting, and so many more.  I
+                        just love learning new things and in Web Development, there is always something new to
+                        learn.
+                    </p>
+                </div>
+                
             </div>
         )
     }
