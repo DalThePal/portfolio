5c26ad44ef6fd8e3ff36fbcb1c90d6c885db453e
DalThePal <dallen.hoyal@gmail.com>
2018-11-21 20:47:23 -0700

    bunch of images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import About from './components/About';
-import Projects from './components/Projects';
+import ProjectRoutes from './components/ProjectRoutes';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 
@@ -33,6 +33,7 @@ class App extends Component {
                 return this.about.current.refs.about.scrollIntoView({ block: 'start', behavior: 'smooth' });
 
             case 'projects':
+                console.log(this.projects.current)
                 return this.projects.current.refs.projects.scrollIntoView({ block: 'start', behavior: 'smooth' });
 
             case 'skills':
@@ -52,7 +53,7 @@ class App extends Component {
                 <NavBar changeScrollTo={this.changeScrollTo} about={this.about} contact={this.contact} />
                 <Header ref={this.picture}/>
                 <About ref={this.about} />
-                <Projects ref={this.projects} />
+                <ProjectRoutes ref={this.projects} />
                 <Skills ref={this.skills} />
                 <Contact ref={this.contact} />
             </div>
