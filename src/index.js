b9c582c9417e9bcd76e1cb8ff5b618f91aaf8359
DalThePal <dallen.hoyal@gmail.com>
2019-06-12 21:45:45 -0600

    modify manifest and servicworker

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import register from './registerServiceWorker';
+
 
 ReactDOM.render(
     <HashRouter>
         <App />
     </HashRouter>
     , document.getElementById('root'));
+
+
+register();
